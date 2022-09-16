06f979d029a758be4b0e797a91e7d8b1a256233f
Nonyfuego <Nonykuti@gmail.com>
2022-09-16 23:56:30 +0100

    make changes in task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# Create a file in /tmp.
-file { '/tmp/holberton':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+# create a file in /tmp
+file { 'holberton':
+    ensure  => file,
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
-
